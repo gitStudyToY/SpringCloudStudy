e1af9db881cf1d2dc0442f8b0fed9680cc29f6cc
yaoyy <1572064767@qq.com>
2018-06-11 16:29:39 +0800

    1111

--- stream-integration-hello/src/main/java/com/example/receiver/App1.java
+++ stream-integration-hello/src/main/java/com/example/receiver/App1.java
@@ -4,7 +4,6 @@ import com.example.channel.SendProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
